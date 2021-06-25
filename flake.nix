7fe1ad3a704950ea50b75d640c1a84056ec723c5
Aaron Janse <aaron@ajanse.me>
2021-06-25 00:13:55 -0700

    update flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,32 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/832ae4311a44ddfee300d54e17268f448b8ea8ea";
-
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.zbak;
-    packages.x86_64-linux.zbak = nixpkgs.legacyPackages.x86_64-linux.callPackage (
-      { lib, makeWrapper, openssh, rustPlatform, zfs }:
-      rustPlatform.buildRustPackage rec {
-        name = "zbak";
-        src = ./.;
-        nativeBuildInputs = [ makeWrapper ];
-        fixupPhase = ''
-          wrapProgram $out/bin/zbak \
-            --set PATH ${lib.makeBinPath [ zfs openssh ]}
-        '';
-        cargoSha256 = "sha256-OgRGWOZin8d/HTbxzJ7vxpTmQHhfs5RlJwhcJPaiMB0=";
-      }
-    ) { };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/832ae4311a44ddfee300d54e17268f448b8ea8ea";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+        packages = flake-utils.lib.flattenTree {
+          zbak = pkgs.callPackage
+            (
+              { lib, makeWrapper, openssh, rustPlatform, zfs }:
+              rustPlatform.buildRustPackage rec {
+                name = "zbak";
+                src = ./.;
+                nativeBuildInputs = [ makeWrapper ];
+                fixupPhase = ''
+                  wrapProgram $out/bin/zbak \
+                    --set PATH ${lib.makeBinPath [ zfs openssh ]}
+                '';
+                cargoSha256 = "sha256-OgRGWOZin8d/HTbxzJ7vxpTmQHhfs5RlJwhcJPaiMB0=";
+              }
+            )
+            { };
+        };
+        defaultPackage = packages.zbak;
+      }
+    );
 }
